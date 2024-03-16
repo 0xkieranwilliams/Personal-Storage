d197cc31be09b5aecfc49a64348c8ae5f1419ec4
bilal-mustafa10 <bilalmustafa041@icloud.com>
2024-03-16 20:58:43 +0000

    Worldcoin integration + css

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,23 @@
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import Image from 'next/image';
+import { signIn } from "next-auth/react";
+import styles from "./homepage.module.css";
 
-export default function IndexPage() {
-  return (
-    <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> with {" "}
-        <a href ="https://worldcoin.org/world-id">World ID</a> for authentication.
-      </p>
-    </Layout>
-  )
+export default function Index() {
+    const handleLogin = () => {
+        console.log('Login');
+        signIn("worldcoin", { callbackUrl: '/admin' });
+    }
+
+    return (
+        <Layout>
+            <div className={styles.heroContainer}>
+                <Image src={require("../assets/img/logo.svg")} width={220} height={220} alt="Logo" />
+                <Image src={require("../assets/img/secure-drawing.svg")} height={400} width={400} alt="Hero"/>
+                <div className={styles.loginButtonContainer}>
+                    <button className={styles.loginButton} onClick={handleLogin}>Sign in using Worldcoin</button>
+                </div>
+            </div>
+        </Layout>
+    );
 }
