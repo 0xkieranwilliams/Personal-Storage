b82692ecc5258434a06ab6b9c175e7846f42cc6a
Kiearn Williams <kieranwilliams@vindico.net>
2024-03-17 02:16:55 +0000

    chgs

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,6 +1,31 @@
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import addresses from "../blockchain-config/contract_address.json";
+import contractABI from "../blockchain-config/abi.json";
+const contractAddress = addresses["314159"];
+console.log(contractABI)
 
 export default function Dashboard() {
+  useEffect(() => {
+    console.log(contractAddress)
+    const fetchDocuments = async () => {
+      // Connect to the Ethereum network
+      const provider = new ethers.BrowserProvider(window.ethereum);
+
+      // Connect to the contract
+      const contract = new ethers.Contract(contractAddress, contractABI, provider);
+      console.log(contract.getDocuments)
+
+      // Call the getDocuments function on the contract
+      const result = await contract["getDocuments(string)"]("Medical Records");
+      console.log(result)
+
+    };
+
+    fetchDocuments();
+  }, []);
+
   return (
     <Layout>
       <h1>This page is protected by NextAuth Middleware</h1>
@@ -12,5 +37,5 @@ export default function Dashboard() {
         </a>
       </p>
     </Layout>
-  )
+  );
 }
